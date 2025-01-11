8238a626f037c56a0b795bacf79b21c38679bef8
Adithya sharma <99574917+Adithya-sharma1316@users.noreply.github.com>
2025-01-11 12:30:48 +0530

    test.js

--- frontend/src/lib/src_lib_api (2).js
+++ frontend/src/lib/src_lib_api (2).js
@@ -1,13 +1,13 @@
-export async function fetchAllPosts() {
-  const response = await api.get('/items/posts', {
-    params: {
-      sort: '-published_date',
-      fields: ['id', 'title', 'author.email', 'published_date'],
-    },
-  });
-  return response.data.data;
-}
+// export async function fetchAllPosts() {
+//   const response = await api.get('/items/posts', {
+//     params: {
+//       sort: '-published_date',
+//       fields: ['id', 'title', 'author.email', 'published_date'],
+//     },
+//   });
+//   return response.data.data;
+// }
 
-export async function deletePost(id) {
-  await api.delete(`/items/posts/${id}`);
-}
+// export async function deletePost(id) {
+//   await api.delete(`/items/posts/${id}`);
+// }
